edb8e15aa8c37b52a3c5ec0fb64d7cf11103bcb1
ariyama1658 <7372964206@g.ecc.u-tokyo.ac.jp>
2024-10-27 19:39:31 +0900

    arucomarkerで動かせます！

--- src/aruco.cpp
+++ src/aruco.cpp
@@ -1,3 +1,4 @@
+//2つのコードマージ
 #include <ros/ros.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <fiducial_msgs/FiducialTransformArray.h>
@@ -5,6 +6,59 @@
 #include <sensor_msgs/Joy.h>
 #include <geometry_msgs/Twist.h>
 
+
+
+
+
+class ArucoNode {
+public:
+    ArucoNode() {
+        // ノードハンドルの初期化
+        nh_ = ros::NodeHandle();
+        // パブリッシャの初期化
+        vel_pub_ = nh_.advertise<geometry_msgs::Twist>("rover_twist", 1);
+        // サブスクライバの初期化
+        sub_ = nh_.subscribe("/fiducial_transforms", 10, &ArucoNode::callback, this);
+    }
+
+    void spin() {
+        ros::Rate r(10);
+        while (ros::ok()) {
+            ros::spinOnce();
+            joyPub();
+            r.sleep();
+        }
+    }
+
+private:
+    void callback(const fiducial_msgs::FiducialTransformArray::ConstPtr& msg) {
+        // コールバック関数の実装
+        if (!msg->transforms.empty()) {
+            const geometry_msgs::Vector3& translation = msg->transforms[0].transform.translation;
+            const geometry_msgs::Quaternion& rotation = msg->transforms[0].transform.rotation;
+            twist_.linear.x = 0;
+            twist_.linear.y = translation.x;
+            twist_.angular.z = rotation.z; // 必要に応じて設定
+        }
+    }
+
+    void joyPub() {
+        vel_pub_.publish(twist_);
+    }
+
+    ros::NodeHandle nh_;
+    ros::Publisher vel_pub_;
+    ros::Subscriber sub_;
+    geometry_msgs::Twist twist_;
+};
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "aruco");
+    ArucoNode aruco_node;
+    aruco_node.spin();
+    return 0;
+}
+/*
 // グローバル変数を定義
 std::vector<double> translation_values;
 std::vector<double> rotation_values;
@@ -23,81 +77,48 @@ void callback(const fiducial_msgs::FiducialTransformArray::ConstPtr& msg) {
     }
 }
 
-class JoyCtrlRover
-{
-public:
-  JoyCtrlRover();
-  void joyPub();
-
-private:
-  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
-  
-
-  ros::NodeHandle n_;
-
   int linear_x_,linear_y_, angular_, safety_;
   double l_scale_, a_scale_;
   ros::Publisher vel_pub_;
-  ros::Subscriber joy_sub_;
+ // ros::Subscriber joy_sub_;
   geometry_msgs::Twist twist_;
 
-};
+  
 
-
-JoyCtrlRover::JoyCtrlRover():
-  linear_x_(1),
-  linear_y_(0),
-  angular_(3),
-  a_scale_(0.8),
-  l_scale_(0.6),
-  safety_(7)
+void sendCallback()
 {
-
-  n_.param("/joycon/axis_linear_x", linear_x_, linear_x_);
-  n_.param("/joycon/axis_linear_y", linear_y_, linear_y_);
-  n_.param("/joycon/axis_angular", angular_, angular_);
-  n_.param("/joycon/scale_angular", a_scale_, a_scale_);
-  n_.param("/joycon/scale_linear", l_scale_, l_scale_);
-  n_.param("/joycon/safety_button", safety_, safety_);
-
-
-  vel_pub_ = n_.advertise<geometry_msgs::Twist>("rover_twist", 1);
-
-
-  joy_sub_ = n_.subscribe<sensor_msgs::Joy>("joy", 1, &JoyCtrlRover::joyCallback, this);
-
-}
-
-void JoyCtrlRover::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
-{
-  if(joy->buttons[safety_]){
-    twist_.angular.z = a_scale_*joy->axes[angular_];
-    twist_.linear.x = l_scale_*joy->axes[linear_x_];
-    twist_.linear.y = l_scale_*joy->axes[linear_y_];
-  }else{
-    twist_.angular.z =0;
-    twist_.linear.x = 0;
-    twist_.linear.y = 0;
-  }
+  //if(\){
+    twist_.angular.z = a_scale_*rotation_values[2];
+    twist_.linear.x = l_scale_*translation_values[0];
+    //twist_.linear.x = l_scale_*0.6;
+    twist_.linear.y = l_scale_*translation_values[2];
+ // }else{
+ //   twist_.angular.z =0;
+ //   twist_.linear.x = 0;
+ //   twist_.linear.y = 0;
+// }
   
 }
 
-void JoyCtrlRover::joyPub()
+void joyPub()
 {
   vel_pub_.publish(twist_);
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "fiducial_transform_listener");
-    ros::init(argc, argv, "joycon");
+    ros::init(argc, argv, "aruco");
     ros::NodeHandle n;
-    ros::Subscriber sub = n.subscribe("/fiducial_transforms", 10, callback);
-    JoyCtrlRover joy_ctrl_mecanumrover;
+    ros::NodeHandle nh;
+    ros::Subscriber sub = nh.subscribe("/fiducial_transforms", 10, callback);
+
+    vel_pub_ = nh.advertise<geometry_msgs::Twist>("rover_twist", 1);
+
     ros::Rate r(10);
     while(n.ok()){
       ros::spinOnce();
-      joy_ctrl_mecanumrover.joyPub();
+      sendCallback();
+      joyPub();
       r.sleep();
     }
 }
-
+*/
